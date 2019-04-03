89dff08e9d3188bd77efacfff0bf1897d0952158
Rotem M <rotemm@wix.com>
2019-04-03 14:51:53 +0300

    install emulator

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -11,14 +11,6 @@ pushd detox/test
 mv node_modules/react-native/ReactAndroid/release.gradle node_modules/react-native/ReactAndroid/release.gradle.bak
 cp extras/release.gradle node_modules/react-native/ReactAndroid/
 
-echo no | avdmanager create avd --force --name Nexus_5X_API_26  --abi x86 --device "Nexus 5X" -k "system-images;android-26;google_apis;x86"
-emulator -avd Nexus_5X_API_26 -no-window -no-audio -debug-init > /dev/null 2>&1 &
-
-echo "Waiting emulator is ready..."
-
-adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
-
-echo "Emulator is ready!"
 run_f "npm run build:android"
 run_f "npm run e2e:android -- --headless"
 # run_f "npm run verify-artifacts:android"
