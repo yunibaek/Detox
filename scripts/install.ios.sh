ec8f28c876ac3ae986403fdb640f8e169bc42352
Leo Natan <leo.natan@outlook.com>
2019-06-30 22:33:07 +0300

    Don’t forget path

--- scripts/install.ios.sh
+++ scripts/install.ios.sh
@@ -2,6 +2,7 @@
 
 $(dirname "$0")/install.sh
 
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 gem install xcpretty
 export CODE_SIGNING_REQUIRED=NO
 HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 brew tap wix/brew
